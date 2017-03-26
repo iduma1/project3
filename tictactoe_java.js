93a63df255cf041a70c0c60745b8f6a3aa206a1a
solorioc <solorioc@kenyon.edu>
2017-03-26 14:32:58 -0400

    Commented out the automatic update function for while working on the server stuff

--- tictactoe_java.js
+++ tictactoe_java.js
@@ -12,15 +12,11 @@
 
 //When the page loads, prompts user to enter name
 
-var XMLHttp;
+var XMLHttp;//Browser-specific var
 var cgiBusy = false;//Stores state of CGI (busy or not busy)
-var pname; //Stores the player's name
+var playerName; //Stores the player's name
+var o
 var boardState="Z,Z,Z,Z,Z,Z,Z,Z,Z";//Stores the current state of the board
-
-/*Uses the callCGI function to update the board every 5 seconds*/
-setInterval(function(){ 
-	callCGI(10);//pos=10 means update the board
-},5000);
 
 /*XMLHttp object definition based on the web browser*/
 if(navigator.appName == "Microsoft Internet Explorer") {
@@ -29,7 +25,19 @@ if(navigator.appName == "Microsoft Internet Explorer") {
     XMLHttp = new XMLHttpRequest();
 }
 
-//Function that will send and receive messages from the ajax program. When pos=10, we are requesting an update of the board.
+window.onload = function getName() {//When the page loads, it requests a user's name
+		playerName = prompt("Enter Your Name", "player");//On entering the website, a player enters their name	
+		p1.innerText = playerName;//Displays the players name on the screen
+		callCGI(10);//Sends a call out to the CGI program that a player has connected
+}
+
+/*Uses the callCGI function to update the board every 3 seconds
+setInterval(function(){ 
+	callCGI(10);//pos=10 means update the board
+},3000);
+*/
+
+//Function that will send and receive messages from the cgi program. When pos=10, program requests board update
 function callCGI(pos) {
 	
 	if (cgiBusy == true) {//If the CGI is busy, this function quits
@@ -45,19 +53,19 @@ function callCGI(pos) {
 						 +"&player=" + "update"
 						 +"&pos=" + pos
 						 ,true);
-		}else {
+		}else {//When the user clicks a box on the board
 		
 		XMLHttp.open("GET", "/cgi-bin/solorioc_tictactoe_ajax.cgi?"
-						 +"&player=" + pname
+						 +"&player=" + playerName
 						 +"&pos=" + pos
 						 ,true);
 		}
 		
-		XMLHttp.onreadystatechange = function() {//Reads in the board state
+		XMLHttp.onreadystatechange = function() {//When the state changes, 
 		
-			if (XMLHttp.readyState === 4) {
-				boardState = XMLHttp.responseText;
-				displayBoard(boardState);
+			if (XMLHttp.readyState === 4) {//On the 4th state, 
+				boardState = XMLHttp.responseText;//The string that the tictactoe_ajax.cpp c-outs is stored
+				updateBoard(boardState);//Use the displayBoard function to update the board on player's screen
 			}
 		}
 	
@@ -69,56 +77,66 @@ function callCGI(pos) {
 					 
 }
 
-/*This function displays the mark in each box based on the message received from CGI program
-  the function displays the appropriate image, depending on if the spot is occupied by an X or an O
-*/
-
-function displayMark(box,sign){
-	if(sign=="X") {	
-		document.getElementById(box).innerHTML='<img hspace="30px" src="https://s3.amazonaws.com/piktochartv2-dev/v2/uploads/8a5899ab-d13e-4cc7-8a12-36279b4e20c1/67fe65d4bb6da9c4303037822efc43a48e7cba44_original.png" height = 140px width = 100px/>';
-	}else if (sign=="O")
-		document.getElementById(box).innerHTML='<img hspace="10px" src="http://i45.tinypic.com/23l1eo.jpg" height = 140px width = 140px />';
-	}
-
-/*This function is what updates the board*/
-function displayBoard(board) {
+/*This function upates the images displayed on the board*/
+function updateBoard(board) {
 	/*Board will be a string like ""O,X,Z,Z,Z,Z,O,X,O" in the case that the game is not over
 	 *Board will be a string like "O,X,Z,Z,Z,Z,O,X,O,WIN,JOHN" in the case that there is a winner
 	 *Board will be a string like "O,X,Z,Z,Z,Z,O,X,O,TIE"in the case that there is a tie
 	 */
 	
-	var splits = board.split(",");//Turns the string into an array separated by the commas
+	var gameState = board.split(",");//Turns the string into an array separated by the commas
 	
-	/*Updates each box in the board*/
-	displayMark('UL',splits[0]);
-	displayMark('UM',splits[1]);
-	displayMark('UR',splits[2]);
-	displayMark('ML',splits[3]);
-	displayMark('MM',splits[4]);
-	displayMark('MR',splits[5]);
-	displayMark('LL',splits[6]);
-	displayMark('LM',splits[7]);
-	displayMark('LR',splits[8]);
+	/*Displays the apropriate mark in each box as given by the boxSign array*/
+	displayMark('UL',gameState[0]);//box 0
+	displayMark('UM',gameState[1]);//box 1
+	displayMark('UR',gameState[2]);//box 2
+	displayMark('ML',gameState[3]);//box 3
+	displayMark('MM',gameState[4]);//box 4
+	displayMark('MR',gameState[5]);//box 5
+	displayMark('LL',gameState[6]);//box 6
+	displayMark('LM',gameState[7]);//box 7
+	displayMark('LR',gameState[8]);//box 8
 	
-	/*Checks the condition of the game*/
+	turnDisplay(gameState);
 	
-	if (splits.length >= 9) {
-		if(splits[9]=="TIE") {//If the TIE string is in the array,
-			gameStatus.innerText = "Tie Game!";
-			document.getElementById("gameOver").style.visibility="visible";
-		}
-		
-		if(splits[9]=="WIN") {//If the WIN string is in the array, 
-			gameStatus.innerText = splits[10]+ " won!";
-			document.getElementById("gameOver").style.visibility="visible";
-		}
-	}
-	
+	isGameOver(gameState);//Checks if the game met a terminal condition
 }
 
-window.onload = function getName() {
-		pname = prompt("Enter Your Name", "player");//On entering the website, a player enters their name	
-		p1.innerText = pname;//Displays the players name on the screen
-		callCGI(10);//Sends a call out to the CGI program that a player has connected
+/*This function displays the mark in each box based on the message received from CGI program
+ *the function displays the appropriate image, depending on if the spot is occupied by an X or an O
+ *if the sign is anything other than X or O, no image is displayed
+*/
+
+function displayMark(box,sign){//Checks the sign and determines which image to display
+	if(sign=="X") {	
+		document.getElementById(box).innerHTML='<img hspace="30px" src="https://s3.amazonaws.com/piktochartv2-dev/v2/uploads/8a5899ab-d13e-4cc7-8a12-36279b4e20c1/67fe65d4bb6da9c4303037822efc43a48e7cba44_original.png" height = 140px width = 100px/>';
+	}else if (sign=="O")
+		document.getElementById(box).innerHTML='<img hspace="10px" src="http://i45.tinypic.com/23l1eo.jpg" height = 140px width = 140px />';
+	}
+	
+function isGameOver(boardState) {//Function checks if the boardState has a terminal condition, such a s a winner or a tie
+
+	if (boardState.length > 11) {//This string
+	
+		document.getElementById("turnDisplay").style.visibility="hidden";//Hides the turn display div
+		
+		if(boardState[12]=="TIE") {//If the TIE string is in the array,
+			gameStatus.innerText = "Tie Game!";//Pushes text into gameStatus div
+			document.getElementById("gameOver").style.visibility="visible";//Reveals the div in the HTML
+		}
+	
+		if(boardState[12]=="WIN") {//If the WIN string is in the array, 
+			gameStatus.innerText = boardState[13]+ " won!";//Pushes text into gameStatus div
+			document.getElementById("gameOver").style.visibility="visible";//Reveals the div in HTML
+		}
+	}else {return;}
+}
+
+function turnDisplay(boardState) {
+	if (boardState[9] == playerName) {
+		turnDisplay.innerText = "Your turn!"
+	} else {
+		turnDisplay.innerText = boardState[10]+"'s Turn"
+	}
 }
 
